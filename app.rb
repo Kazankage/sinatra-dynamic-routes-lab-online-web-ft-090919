05b0ca7e626aeecf5e6108c21bcdb579d221030c
Randel Jiao <randeljiao@gmail.com>
2019-10-22 23:21:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,36 +1,31 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-   get '/reversename/:name' do
-    @name = params[:name]
-    erb :reversename
+ get "/reversename/:name" do
+    params[:name].reverse
   end
 
-  get '/square/:number' do
-    @num = params[:number].to_i
-    erb :square
+  get "/square/:number" do
+    @number = params[:number].to_i
+    "#{@number ** 2}"
   end
 
-  get '/say/:number/:phrase' do
+  get "/say/:number/:phrase" do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    erb :say
+    "#{@phrase}\n" * @number
   end
 
-  get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @w1 = params[:word1]
-    @w2 = params[:word2]
-    @w3 = params[:word3]
-    @w4 = params[:word4]
-    @w5 = params[:word5]
-    erb :saywords
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
-  get '/:operation/:number1/:number2' do
+  get "/:operation/:number1/:number2" do
+    @number1 = params[:number1]
+    @number2 = params[:number2]
     @operation = params[:operation]
-    @num1 = params[:number1].to_i
-    @num2 = params[:number2].to_i
-    erb :operation
+
+    @number1.send(@operation,@number2)
   end
 
 end
